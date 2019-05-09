069c4e32eefd83293bb5a1f73bfe10fe0f90b8a8
nitnek <nitnekb@gmail.com>
2019-05-09 22:41:16 +0200

    feat: add graphql dependencies and basic movies api

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-  res.send('Heyyyyy!!!!!!!!!');
-});
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        movies: [String!]!
+      }
+
+      type RootMutation {
+        createMovie(title: String): String
+      }
+
+      schema{
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      movies: () => {
+        return ['Star wars', 'Batman', 'Kappa'];
+      },
+      createMovie: (args) => {
+        const movieTitle = args.title;
+        return movieTitle;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
