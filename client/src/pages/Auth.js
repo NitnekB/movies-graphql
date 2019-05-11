004d10d126d91134be5ad4d416ed2d66d7c995e6
nitnek <nitnekb@gmail.com>
2019-05-11 19:38:28 +0200

    feat: add authentication form

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
   render() {
     return (
-      <h2>Welcome! Please login</h2>
+      <div className="main-login">
+        <h2>Welcome! Please login</h2>
+        <form className="authForm">
+          <div className="form-control">
+            <label htmlFor="email">E-Mail</label>
+            <input type="email" id="email"></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password"></input>
+          </div>
+          <div className="form-actions">
+            <button type="button">Submit</button>
+            <button type="button">Switch to Sign up</button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
