7810ba5e7209719e3bd59987aa871ff6886f5b8a
nitnek <nitnekb@gmail.com>
2019-05-12 03:09:20 +0200

    feat: add modal to create movie

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import AuthContext from '../context/auth-context';
 
+import '../index.css';
 import './Auth.css';
 
 class AuthPage extends Component {
--- client/src/pages/Movies.js
+++ client/src/pages/Movies.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
 
+import '../index.css';
+import './Movies.css';
+
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+
 class MoviesPage extends Component {
+  state = {
+    creating: false
+  };
+
+  startCreateMovieHandler = () => {
+    this.setState({creating: true});
+  }
+
+  modalConfirmHandler = () => {
+    this.setState({creating: false});
+  }
+
+  modalCancelHandler = () => {
+    this.setState({creating: false});
+  }
+
   render() {
     return (
-      <h2>All Movies</h2>
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && <Modal title="Add movie" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+          <p>Modal content</p>
+        </Modal>}
+        <div>
+          <h2>All Movies</h2>
+          <div className="form-actions">
+            <button onClick={this.startCreateMovieHandler}>Create Movie</button>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
